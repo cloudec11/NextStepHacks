94db90e9b759be5e39c4086d6d50682deebdcf0f
darren4141 <109255883+darren4141@users.noreply.github.com>
2022-08-13 14:51:49 -0400

    Added yearly living expenses

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 function yearlyTuition() {
-    var yearlyCost = document.getElementById("yearly-tuition-cost").value;
+    var yearlyTuition = parseFloat(document.getElementById("yearly-tuition-cost").value);
+    var yearlyLivingExpenses = parseFloat(document.getElementById("yearly-living-expenses").value);
+    var yearlyCost = yearlyTuition + yearlyLivingExpenses;
     var years = document.getElementById("years").value;
     var additional = parseFloat(document.getElementById("meal-plan-cost").value) + parseFloat(document.getElementById("materials-cost").value) + parseFloat(document.getElementById("additional-cost").value);
     var totalCost = parseFloat(yearlyCost * years) + additional;
     var schoolName = document.getElementById("school-name").value;
     
     if(schoolName == ""){
-        schoolName = "your school"
+        schoolName = "school"
     }
 
     document.querySelector('#total-tuition').innerHTML = "The total cost of going to " + schoolName + " for " + years + " years is $" + totalCost;
